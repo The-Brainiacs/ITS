7f092075356da29403bec6e34c8f8a348ece46c8
AsmaSalsabilaYunos <asmasalsabilamy@gmail.com>
2020-07-18 21:32:58 +0800

    get data for notification and login

--- api/db.php
+++ api/db.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE");
         private $host = 'localhost';
         private $user = 'root';
         private $password = '';
-        private $dbname = 'its';
+        private $dbname = 'login';
 
         // Connect
         public function connect(){
